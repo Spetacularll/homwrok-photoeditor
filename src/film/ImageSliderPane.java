ea2e62eb8aefb25516a4a709c5bd2a0e34262a6b
Spetacularll <62170930+Spetacularll@users.noreply.github.com>
2023-05-19 13:03:46 +0800

    done with the thumbnail

--- src/film/ImageSliderPane.java
+++ src/film/ImageSliderPane.java
@@ -5,17 +5,22 @@ import edit.controller.EditPage;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
 import javafx.geometry.Bounds;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Callback;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
@@ -28,6 +33,8 @@ public class ImageSliderPane extends BorderPane {
     private final ImageView imageView;
     private final Timeline timeline;
     private final MediaPlayer mediaPlayer;
+    private double dragStartX;
+    private double dragStartY;
     Button previousButton;
     Button playButton;
 
@@ -37,21 +44,82 @@ public class ImageSliderPane extends BorderPane {
     Button zoomButton;
     Button resetButton;
     HBox buttonsPane;
+    ListView<Image> thumbnailView;
     int currentIndex;
 
-
+    private void initthumbPane() {
+        thumbnailView = new ListView<>();
+        thumbnailView.setOrientation(Orientation.HORIZONTAL);
+        thumbnailView.setPrefHeight(60);
+        thumbnailView.setId("thumbnailBox");
+        thumbnailView.setItems(FXCollections.observableArrayList(images));
+        thumbnailView.setCellFactory(new Callback<ListView<Image>, ListCell<Image>>() {
+            @Override
+            public ListCell<Image> call(ListView<Image> param) {
+                return new ListCell<Image>() {
+                    private ImageView imageView = new ImageView();
+                    @Override
+                    protected void updateItem(Image item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty || item == null) {
+                            setText(null);
+                            setGraphic(null);
+                        } else {
+                            imageView.setImage(item);
+                            imageView.setFitWidth(50);
+                            imageView.setFitHeight(50);
+                            imageView.setPreserveRatio(true);
+                            setGraphic(imageView);
+                        }
+                    }
+                };
+            }
+        });
+        thumbnailView.setOnMouseClicked(event -> {
+            Image selectedItem = thumbnailView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                imageView.setImage(selectedItem);
+                resizeWindow();
+            }
+        });
+    }
     public void initButton() {
-        previousButton = new Button("Previous");
+        previousButton = new Button("");
         previousButton.setOnAction(event -> previousImage());
-        playButton = new Button("Play");
+        playButton = new Button("");
         playButton.setOnAction(event -> play());
-        pauseButton = new Button("Pause");
+        pauseButton = new Button("");
         pauseButton.setOnAction(event -> pause());
-        nextButton = new Button("Next");
+        nextButton = new Button("");
         nextButton.setOnAction(event -> nextImage());
-        editButton = new Button("Edit");
+        editButton = new Button("");
         zoomButton = new Button("Zoom~");
-        resetButton = new Button("Reset");
+        resetButton = new Button("R");
+        Image imageOfPlay = new Image("file:Buttons/Play.png");
+        ImageView IVofPlay = new ImageView(imageOfPlay);
+        IVofPlay.setFitHeight(16);
+        IVofPlay.setFitWidth(16);
+        playButton.setGraphic(IVofPlay);
+        Image imageOfPause = new Image("file:Buttons/Pause.png");
+        ImageView IVOfPause = new ImageView(imageOfPause);
+        IVOfPause.setFitHeight(16);
+        IVOfPause.setFitWidth(16);
+        pauseButton.setGraphic(IVOfPause);
+        Image imageOfpre = new Image("file:Buttons/Left.png");
+        ImageView IVOfpre = new ImageView(imageOfpre);
+        IVOfpre.setFitHeight(16);
+        IVOfpre.setFitWidth(16);
+        previousButton.setGraphic(IVOfpre);
+        Image imageofnext   = new Image("file:Buttons/Right.png");
+        ImageView IVofnext = new ImageView(imageofnext);
+        IVofnext.setFitHeight(16);
+        IVofnext.setFitWidth(16);
+        nextButton.setGraphic(IVofnext);
+        Image imageofedit   = new Image("file:Buttons/draw.png");
+        ImageView IVofedit = new ImageView(imageofedit);
+        IVofedit.setFitHeight(16);
+        IVofedit.setFitWidth(16);
+        editButton.setGraphic(IVofedit);
         editButton.setOnAction(event ->{
             currentIndex = images.indexOf(imageView.getImage());
             if(currentIndex<=imageArrayList.size()&&currentIndex!=-1){
@@ -144,6 +212,17 @@ public class ImageSliderPane extends BorderPane {
         mediaPlayer = new MediaPlayer(backgroundMusic);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayer.setVolume(0.5);
+        imageView.setOnMousePressed(event -> {
+            dragStartX = event.getSceneX() - imageView.getTranslateX();
+            dragStartY = event.getSceneY() - imageView.getTranslateY();
+        });
+
+        imageView.setOnMouseDragged(event -> {
+            imageView.setTranslateX(event.getSceneX() - dragStartX);
+            imageView.setTranslateY(event.getSceneY() - dragStartY);
+        });
+        initthumbPane();
+        setTop(thumbnailView);
         initButton();
         play();
         setCenter(imageView);
@@ -184,10 +263,11 @@ public class ImageSliderPane extends BorderPane {
 
 
     private void resizeWindow() {
-        imageView.fitWidthProperty().bind(widthProperty());
+        imageView.fitWidthProperty().bind(this.widthProperty());
         Node bottomNode = lookup("#buttonsPane");
-        if (bottomNode != null) {
-            imageView.fitHeightProperty().bind(heightProperty().subtract(((HBox) bottomNode).getHeight()));
+        Node topNode = lookup("#thumbnailBox");
+        if (bottomNode != null && topNode != null) {
+            imageView.fitHeightProperty().bind(heightProperty().subtract(((HBox) bottomNode).getHeight()).subtract(((ListView<Image>) topNode).getHeight()));
         }
     }
 
